3f2659b87937228739c6887ff8070b52f805a91f
liann <lillyannmaka@gmail.com>
2022-05-14 04:00:59 -0700

    updated the component folder

---
+++ src/components/Results.vue
@@ -0,0 +1,34 @@
+<template>
+  <p >Reaction Time: {{ score }} ms</p>
+  <p class="rank"> {{ rank}}</p>
+</template>
+
+<script>
+export default {
+props:['score'],
+data (){
+    return {
+        rank: null
+    }
+},
+mounted (){
+    if(this.score < 200){
+        this.rank = 'fire fingers'
+    }
+    else if (this.score < 400){
+        this.rank = 'medium'
+    }
+    else{
+        this.rank = 'slow'
+    }
+}
+}
+</script>
+
+<style>
+  .rank {
+      font-size: 1.4em;
+      color: #0faf87;
+      font-weight: bold;
+  }
+</style>
